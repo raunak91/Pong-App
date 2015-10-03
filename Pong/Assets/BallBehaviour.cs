c8c85590726e84c29582254eb6f127e5b01ea952
meeya <ynafey@gmail.com>
2015-10-03 16:05:45 +0530

    Minor fixes

--- Pong/Assets/BallBehaviour.cs
+++ Pong/Assets/BallBehaviour.cs
@@ -2,35 +2,39 @@
 using System.Collections;
 
 public class BallBehaviour : MonoBehaviour {
+    public Vector3 startingPosition;
     public float standardForce;
     public float upForce;
 
-    private bool firstForceFlag = true;
+    private bool isStationaryFlag = true;
     private bool forceRightFlag = false;
     private bool forceLeftFlag = false;
 
+    private Rigidbody rigidbody;
+
     void Awake() {
-        
+        this.rigidbody = this.gameObject.GetComponent<Rigidbody>();
     }
     
 	void Start () {
+        this.transform.position = this.startingPosition;
 	}
 
 	void Update () {
-        if (firstForceFlag) {
+        if (isStationaryFlag) {
             if (Input.GetKeyDown(KeyCode.Z)) {
-                this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-                this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, standardForce));
-                firstForceFlag = false;
+                this.rigidbody.useGravity = true;
+                this.rigidbody.AddForce(new Vector3(0, 0, standardForce), ForceMode.Impulse);
+                isStationaryFlag = false;
             }
         } else {
             if (forceRightFlag) {
-                this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, upForce, 2 * standardForce));
+                this.rigidbody.AddForce(new Vector3(0, upForce, 2 * standardForce), ForceMode.Impulse);
                 this.forceRightFlag = false;
             }
 
             if (forceLeftFlag) {
-                this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, upForce, -2 * standardForce));
+                this.rigidbody.AddForce(new Vector3(0, upForce, -2 * standardForce), ForceMode.Impulse);
                 this.forceLeftFlag = false;
             }
 
@@ -40,6 +44,15 @@ public class BallBehaviour : MonoBehaviour {
 
             if (Input.GetKeyDown(KeyCode.Period)) {
                 this.forceLeftFlag = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                this.rigidbody.useGravity = false;
+                this.rigidbody.velocity = Vector3.zero;
+                this.rigidbody.angularVelocity = Vector3.zero;
+                this.transform.position = this.startingPosition;
+
+                this.isStationaryFlag = true;
             }
         }
 	}
